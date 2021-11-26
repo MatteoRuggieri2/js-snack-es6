d99fb81341abfb93374a1c1c3c257c0650939fc3
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-26 16:17:39 +0100

    Es.1 - Leggo la bici più leggera

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -4,7 +4,7 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
 
-
+// Creo l'array
 const bikeArray = 
 [
     {
@@ -17,6 +17,26 @@ const bikeArray =
     },
     {
         nome: 'Cannondele SuperSix EVO Carbon Disc Ultegra',
-        peso: 7
+        peso: 6
     }
 ]
+
+
+// Lettura e destructuring degli oggetti
+for (let i = 0; i < bikeArray.length; i++) {
+    const singleBike = bikeArray[i]
+
+    // Destucturing degli oggetti
+    const {nome, peso} = singleBike;
+
+    // Inizializzo la variabile con il primo oggetto dell'array per confrontarlo
+    let lightestBike = bikeArray[0].peso
+    console.log('prima: ', lightestBike)
+
+    if (peso < lightestBike) {
+        lightestBike = peso
+    }
+    console.log('Dopo: ', lightestBike)
+
+
+}
