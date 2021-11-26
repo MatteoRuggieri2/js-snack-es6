c0038f85edffe450301e65fac7e9cf41732337e1
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-26 18:19:01 +0100

    Es.2 - Aggiunto al nuovo array nome e falli subiti

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -32,6 +32,7 @@ const footballTeamsArray =
 // Genero numeri random per punti fatti e falli subiti
 let teamScore;
 let foulsSuffered;
+const newFootballTeamsArray = [];
 
 for (let i = 0; i < footballTeamsArray.length; i++) {
     let team = footballTeamsArray[i];
@@ -44,40 +45,23 @@ for (let i = 0; i < footballTeamsArray.length; i++) {
     foulsSuffered = getRndInteger(0, 20);
     console.log('Fouls Suffered: ', foulsSuffered);
 
+    // Assegno i dati agli oggetti
     team.puntiFatti = teamScore;
     team.falliSubiti = foulsSuffered;
 
-    console.log(team.puntiFatti);
-    console.log(team.falliSubiti);
+    // Usando la destrutturazione, creo un nuovo array e lo popolo solo con i nomi delle squadre e i falli subiti
+    const {nome, falliSubiti} = team;
+
+    newFootballTeamsArray.push({
+        nome: nome,
+        falliSubiti: falliSubiti
+    });
+    
+    console.log('Nuovo array: ', newFootballTeamsArray);
+
 }
 
 
-
-
-
-
-
-
-// Creo un nuovo array usando la destrutturazione, e lo popolo solo con i nomi delle squadre e i falli subiti
-// const newFootballTeamsArray = [];
-// for (let i = 0; i < footballTeamsArray.length; i++) {
-//     let singleTeam = footballTeamsArray[i];
-
-//     const {nome, falliSubiti} = singleTeam
-
-//     let newTeam = (`
-//     {
-//         nome: ${nome},
-//         falliSubiti: ${falliSubiti}
-//     },
-//     `)
-
-//     newFootballTeamsArray.push(newTeam);
-
-//     console.log(newFootballTeamsArray);
-
-
-// }
 
 // FUNCTION
 function getRndInteger(min, max) {
