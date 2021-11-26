c79329ff335fc9bb24c0a5e6aa5404fc44e6d371
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-26 15:38:20 +0100

    Es.1 - Creato array di oggetti

---
+++ snack-1/js/script.js
@@ -0,0 +1,22 @@
+// SNACK 1
+
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+
+
+const bikeArray = 
+[
+    {
+        nome: 'Trek Émonda SLR 9 eTap',
+        peso: 7
+    },
+    {
+        nome: 'Giant Propel Advanced PRO DISC 0',
+        peso: 8
+    },
+    {
+        nome: 'Cannondele SuperSix EVO Carbon Disc Ultegra',
+        peso: 7
+    }
+]
