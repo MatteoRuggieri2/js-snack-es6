6e06e06118610ff2f905c01ab439dc40fe5bc95e
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-29 16:38:03 +0100

    Es.3 - Aggiunti element all'array - forEach

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -5,3 +5,17 @@
 // e dati due numeri min e max (min più piccolo di max).
 // Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
 // Eseguiamo questo esercizio prima con forEach e poi con filter.
+
+
+// ESERCIZIO CON 'forEach'
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+const min = 1;
+const max = 5;
+
+const newNameArray = [];
+myArray.forEach((element, index, array) => {
+    if( index > min && index < max ) {
+        newNameArray.push(element);
+    }
+});
+console.log('newNameArray: ', newNameArray)
