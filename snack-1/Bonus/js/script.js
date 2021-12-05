6c942bcd3690159162c15b0e6ebb80082785cfc5
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-05 17:11:42 +0100

    Bonus - Creato template

---
+++ snack-1/Bonus/js/script.js
@@ -0,0 +1,51 @@
+// SNACK 1 - BONUS
+
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+
+// Creo l'array
+const bikeArray = 
+[
+    {
+        nome: 'Trek Émonda SLR 9 eTap',
+        peso: 7
+    },
+    {
+        nome: 'Giant Propel Advanced PRO DISC 0',
+        peso: 8
+    },
+    {
+        nome: 'Cannondele SuperSix EVO Carbon Disc Ultegra',
+        peso: 6
+    }
+]
+
+
+// Inizializzo la variabile con il primo oggetto dell'array per confrontarlo
+let lightestBike = bikeArray[0]
+
+// Lettura e destructuring degli oggetti
+for (let i = 0; i < bikeArray.length; i++) {
+    const singleBike = bikeArray[i]
+    
+    // Se il peso della bici è inferiore di quela di default lo sovrascrivo 
+    if (singleBike.peso < lightestBike.peso) {
+        lightestBike = singleBike
+    }
+    
+}
+
+// Seleziono il container
+const container = document.getElementById('container');
+
+// Destucturing degli oggetti
+const {nome, peso} = lightestBike;
+
+// Stampo le Bike Features
+// container.innerHTML += `
+// <ul id="bike-fetures-list">
+//     <li>Nome: ${nome}</li>
+//     <li>Peso: ${peso} kg</li>
+// </ul>
+// `
