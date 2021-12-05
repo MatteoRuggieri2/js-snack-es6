3ca909083975120d8cbe0a979b58c43a62364542
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-05 22:58:10 +0100

    Bonus - Inserito le card dinamicamente

--- snack-1/Bonus/js/script.js
+++ snack-1/Bonus/js/script.js
@@ -9,30 +9,51 @@ const bikeArray =
 [
     {
         nome: 'Trek Émonda SLR 9 eTap',
-        peso: 7
+        peso: 7,
+        image: 'img/trek-emonda-slr-9-etap.png'
     },
     {
         nome: 'Giant Propel Advanced PRO DISC 0',
-        peso: 8
+        peso: 8,
+        image: 'img/giant-propel-advanced-pro-disc-0.jpg'
     },
     {
         nome: 'Cannondele SuperSix EVO Carbon Disc Ultegra',
-        peso: 6
+        peso: 6,
+        image: 'img/cannondele-supersix-evo-carbon.webp'
     }
+    
 ]
 
+// seleziono bike-container
+const bikeContainer = document.querySelector('.bike-container');
 
 // Inizializzo la variabile con il primo oggetto dell'array per confrontarlo
 let lightestBike = bikeArray[0]
 
+
+
 // Lettura e destructuring degli oggetti
 for (let i = 0; i < bikeArray.length; i++) {
     const singleBike = bikeArray[i]
+
+let activeClass = '';
+
+
     
     // Se il peso della bici è inferiore di quela di default lo sovrascrivo 
     if (singleBike.peso < lightestBike.peso) {
         lightestBike = singleBike
+        activeClass = 'active';
     }
+
+    bikeContainer.innerHTML += `
+    <!-- Single Bike -->
+    <div class="card ${activeClass}">
+        <img src="${singleBike.image}" alt="${singleBike.nome}">
+        <h2>${singleBike.nome}</h2>
+    </div>
+    `
     
 }
 
@@ -43,9 +64,9 @@ const container = document.getElementById('container');
 const {nome, peso} = lightestBike;
 
 // Stampo le Bike Features
-// container.innerHTML += `
-// <ul id="bike-fetures-list">
-//     <li>Nome: ${nome}</li>
-//     <li>Peso: ${peso} kg</li>
-// </ul>
-// `
+container.innerHTML += `
+<ul id="bike-fetures-list">
+    <li>Nome: ${nome}</li>
+    <li>Peso: ${peso} kg</li>
+</ul>
+`
