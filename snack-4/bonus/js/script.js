54dbe6421d9bf8ea06083aaca158648267301d9c
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-08 22:51:41 +0100

    Bonus terminato

--- snack-4/bonus/js/script.js
+++ snack-4/bonus/js/script.js
@@ -21,59 +21,80 @@ let students = [
         { name: 'Francesca', id: 120, grades: 84 },
 ];
 
-
+// Seleziono la lista degli studenti
 const studentNameList = document.getElementById('student-list')
 
 for (let i = 0; i < students.length; i++) {
     let singleStudentName = students[i].name.toUpperCase();
-    console.log('singleStudentName: ', singleStudentName)
     studentNameList.innerHTML += `
     <li>${singleStudentName}</li>
     `
 }
 
+// Al click del bottone "voti >= 70" mostra nella lista di destra gli studenti
+
+// Seleziono il bottone "voti >= 70"
+const button70 = document.getElementById('button-70');
+button70.addEventListener('click', studentFilter70);
+
+// Seleziono il bottone "voti >= 70 e id >= 120"
+const button70And120 = document.getElementById('button-70-120');
+button70And120.addEventListener('click', studentFilter70And120);
+
+const filteredStudentList = document.getElementById('filtered-list');
+const filteredListTitle = document.getElementById('filtered-list-title');
+let studentTitle = document.getElementById('student-title');
 
 
-// Con filter creo un nuovo array dove inserisco gli studenti solo se hanno grades > 70.
-const students70array = students.filter((element, index, array) => {
-    if( element.grades >= 70 ) {
-        return true;
+
+
+
+
+
+
+
+// FUNCTION
+
+// Questa funzione filtra gli studenti con voto >= 70,
+// e li inserisce nella lista degli studenti filtrati.
+function studentFilter70() {
+
+    filteredStudentList.innerHTML = ``
+    studentTitle.innerHTML = `Studenti con`
+    filteredListTitle.innerHTML = `voti > 70:`
+
+    // Con filter creo un nuovo array dove inserisco gli studenti solo se hanno grades >= 70.
+    const students70array = students.filter((element, index, array) => {
+         return element.grades >= 70;
+    });
+
+    for (let i = 0; i < students70array.length; i++) {
+        let singleStudent70 = students70array[i].name;
+        filteredStudentList.innerHTML += `
+        <li>${singleStudent70}</li>
+        `
     }
-    // Versione abbreviata
-    // return element.grades >= 70;
-});
-console.log('students70array: ', students70array)
 
+}
 
+// Questa funzione filtra gli studenti con voto >= 70 e id >= 120,
+// e li inserisce nella lista degli studenti filtrati.
+function studentFilter70And120() {
 
-// Con filter creo un nuovo array dove inserisco gli studenti solo se hanno grades > 70 e id > 120.
-const students70And120Array = students.filter((element, index, array) => {
-    if( element.grades >= 70 && element.id >= 120 ) {
-        return true;
+    filteredStudentList.innerHTML = ``
+    studentTitle.innerHTML = `Studenti con`
+    filteredListTitle.innerHTML = `voti > 70 e 120:`
+
+    // Con filter creo un nuovo array dove inserisco gli studenti solo se hanno grades >= 70 e id >= 120.
+    const students70And120Array = students.filter((element, index, array) => {
+        return element.grades >= 70 && element.id >= 120;
+    });
+
+    for (let i = 0; i < students70And120Array.length; i++) {
+        let singleStudent70And120 = students70And120Array[i].name;
+        filteredStudentList.innerHTML += `
+        <li>${singleStudent70And120}</li>
+        `
     }
-    // Versione abbreviata
-    // return element.grades >= 70 && element.id >= 120;
-});
-console.log('students70And120Array: ', students70And120Array)
 
-// let studentList = document.getElementById(student-list);
-// console.log(studentList)
-// // Stampo tutti gli studenti nella pagina a sinistra
-// for (let i = 0; i < students.length; i++) {
-//     studentList.innerHTML += `
-//     <ul>
-//         <li>ciao</li>
-//         <li>ciao</li>
-//         <li>ciao</li>
-//         <li>ciao</li>
-//         <li>ciao</li>
-//         <li>ciao</li>
-//         <li>ciao</li>
-//     </ul>
-    // `
-// }
-
-// Al click del bottone 70, stampo tutti glistudenti con più di 70
-
-// Al click del bottone 70-100, cancello quello che c'era prima e stampo
-// tutti glistudenti con più di 70 e 100
+}
